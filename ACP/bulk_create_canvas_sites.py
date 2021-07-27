9a75cb2e2a6900db0b0883c4bfc04e007218163a
Ben Rosen <benrosen@upenn.edu>
2021-07-27 15:42:09 -0400

    remove unused features and limit to SEAS

--- ACP/bulk_create_canvas_sites.py
+++ ACP/bulk_create_canvas_sites.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from canvas.api import get_canvas
 from canvasapi.exceptions import CanvasException
-from course.models import Course, Request, User
+from course.models import Course, Request, User, School
 from course.tasks import create_canvas_sites
 
 from .logger import canvas_logger, crf_logger
@@ -19,6 +19,8 @@ def get_unrequested_courses(year_and_term):
     term = year_and_term[-1]
     year = year_and_term[:-1]
 
+    SEAS = School.objects.get(abbreviation="SEAS")
+
     unrequested_courses = Course.objects.filter(
         course_term=term,
         year=year,
@@ -26,6 +28,7 @@ def get_unrequested_courses(year_and_term):
         requested_override=False,
         primary_crosslist="",
         course_schools__visible=True,
+        course_schools=SEAS,
     )
 
     total_unrequested = len(unrequested_courses)
@@ -46,10 +49,9 @@ def should_request(sis_id, test=False):
         return True
 
 
-def request_course(course, copy_site):
+def request_course(course):
     request = Request.objects.create(
         course_requested=course,
-        copy_from_course=copy_site,
         additional_instructions=(
             "Request automatically generated; contact Courseware Support"
             " for more information."
@@ -65,87 +67,41 @@ def request_course(course, copy_site):
     print("\t* Request complete.")
 
 
-def enable_lti(canvas_id, tool, label=False, test=False):
-    print(f"\t> Enabling {tool}...")
-
-    try:
-        canvas = get_canvas(test)
-        canvas_site = canvas.get_course(canvas_id)
-        tabs = canvas_site.get_tabs()
-
-        if label:
-            tool_tab = next(filter(lambda tab: tab.label == tool, tabs), None)
-        else:
-            tool_tab = next(filter(lambda tab: tab.id == tool, tabs), None)
-
-        if tool_tab.visibility != "public":
-            tool_tab.update(hidden=False, position=3)
-            print(f"\t* Enabled {tool_tab.label}.")
-        else:
-            print(f"\t* {tool_tab.label} already enabled for course.")
-    except Exception as error:
-        print(f"\t* ERROR: Failed to enable {tool} ({error}).")
-        canvas_logger.info(f"ERROR: Failed to enable {tool} for {canvas_id} ({error}).")
-
-
-def copy_content(canvas_id, source_site, test=False):
-    print(f"\t> Copying course content from {source_site}...")
-
-    try:
-        canvas = get_canvas(test)
-        canvas_site = canvas.get_course(canvas_id)
-        canvas_site.create_content_migration(
-            migration_type="course_copy_importer",
-            settings={"[source_course_id": source_site},
-        )
-        print("\t* Created content migration.")
-    except Exception as error:
-        print(f"\t* ERROR: Failed to create content migration ({error}).")
-        canvas_logger.info(f"ERROR: Failed to find site {canvas_id} ({error})")
-
-
-def config_site(canvas_id, capacity, publish, tools, source_site, label, test):
-    if source_site:
-        copy_content(canvas_id, source_site, test)
-
-    if tools:
-        for tool in tools:
-            enable_lti(canvas_id, tool, label, test)
-
-    config = {}
-
-    if capacity:
-        config["storage_quota_mb"] = capacity
-
-    if publish:
-        config["event"] = "offer"
-
-    if publish or capacity:
-        print(f"\t> Updating with config: {config}...")
+def enable_tools(canvas_id, tools, label, test):
+    for tool in tools:
+        print(f"\t> Enabling {tool}...")
 
         try:
             canvas = get_canvas(test)
             canvas_site = canvas.get_course(canvas_id)
-            canvas_site.update(course=config)
-            print("\t* Updated complete.")
+            tabs = canvas_site.get_tabs()
+
+            if label:
+                tool_tab = next(filter(lambda tab: tab.label == tool, tabs), None)
+            else:
+                tool_tab = next(filter(lambda tab: tab.id == tool, tabs), None)
+
+            if tool_tab.visibility != "public":
+                tool_tab.update(hidden=False, position=3)
+                print(f"\t* Enabled {tool_tab.label}.")
+            else:
+                print(f"\t* {tool_tab.label} already enabled for course.")
         except Exception as error:
-            print(f"\t* ERROR: Failed to update course ({error}).")
-            canvas_logger.info(f"ERROR: Failed to update site {canvas_id} ({error}).")
+            print(f"\t* ERROR: Failed to enable {tool} ({error}).")
+            canvas_logger.info(
+                f"ERROR: Failed to enable {tool} for {canvas_id} ({error})."
+            )
 
 
 def bulk_create_canvas_sites(
     year_and_term,
-    copy_site="",
-    config=False,
-    capacity=2,
-    publish=False,
+    enable=True,
     tools={
         "context_external_tool_90311": "Class Recordings",
         "context_external_tool_231623": "Zoom",
         "context_external_tool_132117": "Gradescope",
     },
-    label=False,
-    source_site=1599746,
+    label=True,
     test=False,
 ):
     if type(tools) == dict and label:
@@ -165,7 +121,7 @@ def bulk_create_canvas_sites(
         if should_request(sis_id):
             try:
                 print("\t> Requesting course...")
-                request_course(course, copy_site)
+                request_course(course)
 
                 print("\t> Creating Canvas site...")
                 create_canvas_sites(test=test, verbose=False)
@@ -182,11 +138,9 @@ def bulk_create_canvas_sites(
                     )
                     continue
 
-                if config:
+                if enable:
                     canvas_id = request.canvas_instance.canvas_id
-                    config_site(
-                        canvas_id, capacity, publish, tools, source_site, label, test
-                    )
+                    enable_tools(canvas_id, tools, label, test)
             except Exception as error:
                 print(f"\t* ERROR: Failed to create site. ({error})")
                 crf_logger.info(f"ERROR: Failed to create site for {course} ({error}).")
