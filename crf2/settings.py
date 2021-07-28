f8110d16c10019c89b1a33d90543dbe19dc1e404
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-07-28 10:40:08 -0400

    allow for multiple schools in bulk create

--- crf2/settings.py
+++ crf2/settings.py
@@ -21,7 +21,6 @@ config.read("config/config.ini")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("basedir", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
