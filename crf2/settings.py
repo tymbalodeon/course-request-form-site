6d0ef1662b9400a52ab439657ebab1148201a2f0
Ben Rosen <benrosen@upenn.edu>
2021-08-04 08:58:25 -0400

    attempt to up the sqlite timeout

--- crf2/settings.py
+++ crf2/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "OPTIONS": {"timeout": 10},
     }
 }
 CORS_ORIGIN_ALLOW_ALL = True
