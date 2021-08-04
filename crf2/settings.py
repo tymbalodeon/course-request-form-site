d86286821e529909e7480f214526c693a83e76d1
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-08-04 10:30:00 -0400

    move call to get courses to appropriate moment

--- crf2/settings.py
+++ crf2/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "OPTIONS": {"timeout": 10},
+        "OPTIONS": {"timeout": 20},
     }
 }
 CORS_ORIGIN_ALLOW_ALL = True
