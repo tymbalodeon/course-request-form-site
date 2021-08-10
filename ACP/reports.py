176a8d8e5159aefa876e543d328e8092ef70911d
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-08-09 23:49:21 -0400

    allow grad min as argument

--- ACP/reports.py
+++ ACP/reports.py
@@ -1,11 +1,11 @@
 from course.models import Course
 
-GRADUATE_COURSE_MINIMUM_NUMBER = 500
 
-
-def count_canvas_sites(year_and_term, separate=True):
+def count_canvas_sites(
+    year_and_term, separate=True, graduate_course_minimum_number=500
+):
     def is_grad_course(course):
-        return int(course.course_number) >= GRADUATE_COURSE_MINIMUM_NUMBER
+        return int(course.course_number) >= graduate_course_minimum_number
 
     def is_numeric_course_number(course):
         try:
