f197ae51e837a4a787738071a740e34a4fb05562
Ben Rosen <benrosen@upenn.edu>
2021-09-13 14:21:14 -0400

    finish make requests objects function

--- ACP/helpers.py
+++ ACP/helpers.py
@@ -1,2 +1,15 @@
+from os import mkdir
+from pathlib import Path
+
+
 def separate_year_and_term(year_and_term):
     return year_and_term[:-1], year_and_term[-1]
+
+
+def get_data_directory():
+    DATA_DIRECTORY = Path.cwd() / "data"
+
+    if not DATA_DIRECTORY.exists():
+        mkdir(DATA_DIRECTORY)
+
+    return DATA_DIRECTORY
