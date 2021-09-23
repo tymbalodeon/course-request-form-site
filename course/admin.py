d73c896ce4bd00c1be5caa64e8858c4fdbe5f55e
Ben Rosen <benrosen@upenn.edu>
2021-09-23 13:13:58 -0400

    update admin

--- course/admin.py
+++ course/admin.py
@@ -1,53 +1,36 @@
-from admin_auto_filters.filters import AutocompleteFilter
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from django.db.models import (
-    Case,
     Count,
     DateTimeField,
     Exists,
-    F,
     IntegerField,
     Max,
     Min,
     OuterRef,
-    Subquery,
     Sum,
-    Value,
-    When,
 )
 from django.db.models.functions import Trunc
 
 from .models import *
 
-"""
-can implement EXACT search by instructor username, course code, or course title
-
-"""
-
 
 class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
-
         meta = self.model._meta
         field_names = [field.name for field in meta.fields]
-
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
         writer = csv.writer(response)
-
         writer.writerow(field_names)
-        for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        for query_object in queryset:
+            writer.writerow([getattr(query_object, field) for field in field_names])
 
         return response
 
     export_as_csv.short_description = "Export Selected"
-
-
-# admin.site.site_url = settings.URL_PREFIX
 
 
 class AdditionalEnrollmentInline(admin.StackedInline):
@@ -70,7 +53,6 @@ class CourseAdmin(admin.ModelAdmin):
 
     list_filter = ("course_activity", "course_term", "course_schools")
     search_fields = ("instructors__username", "course_code", "course_name")
-    # readonly_fields = ['created','updated','owner','course_code','course_section','course_term','year','course_number','course_subject'] # maybe add requested to here.
     autocomplete_fields = [
         "crosslisted",
         "instructors",
@@ -153,16 +135,9 @@ class CourseAdmin(admin.ModelAdmin):
         else:
             return ["created", "updated", "owner", "course_code", "request"]
 
-    def save_model(self, request, obj, form, change):
-        # print("checkin save")
+    def save_model(self, request, obj):
         obj.owner = request.user
         obj.save()
-
-
-"""
-can implement EXACT search by user that made the request or masquerade, course code
-
-"""
 
 
 class RequestAdmin(admin.ModelAdmin):
@@ -182,7 +157,6 @@ class RequestAdmin(admin.ModelAdmin):
     readonly_fields = ["created", "updated", "masquerade", "additional_sections"]
     inlines = [AdditionalEnrollmentInline]
     autocomplete_fields = ["owner", "course_requested", "canvas_instance"]
-
     fieldsets = (
         (
             None,
@@ -211,15 +185,15 @@ class RequestAdmin(admin.ModelAdmin):
     def additional_sections(self, instance):
         return instance.additional_sections.course_code
 
-    def requestors(self, object):
-        if object.masquerade != "":
-            masquerade = f"{object.owner.username} ({object.masquerade})"
+    def requestors(self, obj):
+        if obj.masquerade != "":
+            masquerade = f"{obj.owner.username} ({obj.masquerade})"
         else:
-            masquerade = object.owner.username
+            masquerade = obj.owner.username
 
         return masquerade
 
-    def save_model(self, request, obj, form, change):
+    def save_model(self, request, obj):
         obj.save()
 
 
@@ -245,15 +219,15 @@ class AutoAddAdmin(admin.ModelAdmin):
 def get_next_in_date_hierarchy(request, date_hierarchy):
     if date_hierarchy + "__day" in request.GET:
         return "hour"
-    if date_hierarchy + "__month" in request.GET:
+    elif date_hierarchy + "__month" in request.GET:
         return "day"
-    if date_hierarchy + "__year" in request.GET:
+    elif date_hierarchy + "__year" in request.GET:
         return "week"
-    return "month"
+    else:
+        return "month"
 
 
 class RequestSummaryAdmin(admin.ModelAdmin):
-
     change_list_template = "admin/request_summary_change_list.html"
     date_hierarchy = "created"
     list_filter = ("course_requested__course_term",)
@@ -264,27 +238,19 @@ class RequestSummaryAdmin(admin.ModelAdmin):
             extra_context=extra_context,
         )
         try:
-            qs = response.context_data["cl"].queryset
+            query_set = response.context_data["cl"].queryset
         except (AttributeError, KeyError):
             return response
-
-        """
-        issues with multisection
-        """
-        multisectionExists = qs.filter(
+        multisectionExists = query_set.filter(
             course_requested=OuterRef("pk"), additional_sections__isnull=False
         )
-
         metrics = {
             "total": Count("course_requested", distinct=True),
             "ares": Count("reserves", filter=Q(reserves=True)),
             "multisection": Sum(
                 Exists(multisectionExists), output_field=IntegerField()
             ),
-            # F('additional_sections'),#Sum(Case(When(~Q(additional_sections=None), then=Value(1)),default=0,output_field=IntegerField(),distinct=True)),
-            "content_copy": Count(
-                "copy_from_course", filter=~Q(copy_from_course="")
-            ),  # ,filter=~Q(copy_from_course='None')&~Q(copy_from_course='')),
+            "content_copy": Count("copy_from_course", filter=~Q(copy_from_course="")),
             "not_completed": Count(
                 "status",
                 filter=Q(
@@ -296,37 +262,22 @@ class RequestSummaryAdmin(admin.ModelAdmin):
                         "LOCKED",
                     ]
                 ),
-            ),  # Sum(Case(When(~Q(status='COMPLETED'), then=1),output_field=IntegerField(),)),
-            #'total_requests': Sum(''),
-            #'multisection':Count(F('additional_sections'),filter=Q(additional_sections__isnull=False)),
-            # filter=Q(additional_sections__isnull=True),distinct=True),#/Count('additional_sections', distinct=True)),
+            ),
         }
 
-        #
         response.context_data["summary"] = list(
-            qs.values(
-                "course_requested__course_schools__abbreviation"
-            ).annotate(  #'course_requested','status',)#,'course_requested')
-                **metrics
-            )
-            # .annotate(multisection=Case(When(mutlisection_count__gt=0,then=Value(1)),default=Value(0),output_field=IntegerField()))
+            query_set.values("course_requested__course_schools__abbreviation")
+            .annotate()
             .order_by("course_requested__course_schools__abbreviation")
-            # .annotate(multisection=Count('additional_sections',filter=Q(additional_sections__isnull=False),distinct=True))
         )
-
-        ##### LAST ROW  #####
-        # metrics = metrics.pop('multisection')
-
-        response.context_data["summary_total"] = dict(qs.aggregate(**metrics))
-
-        ##### BAR CHART #####
+        response.context_data["summary_total"] = dict(query_set.aggregate(**metrics))
         period = get_next_in_date_hierarchy(
             request,
             self.date_hierarchy,
         )
         response.context_data["period"] = period
         summary_over_time = (
-            qs.annotate(
+            query_set.annotate(
                 period=Trunc(
                     "created",
                     period,
@@ -337,7 +288,6 @@ class RequestSummaryAdmin(admin.ModelAdmin):
             .annotate(total=Count("course_requested"))
             .order_by("period")
         )
-
         summary_range = summary_over_time.aggregate(
             low=Min("total"),
             high=Max("total"),
@@ -358,12 +308,8 @@ class RequestSummaryAdmin(admin.ModelAdmin):
         return response
 
 
-# unregister old user admin
 admin.site.unregister(User)
-# register new user admin
 admin.site.register(User, UserAdmin)
-
-
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Request, RequestAdmin)
 admin.site.register(Notice)
@@ -375,9 +321,3 @@ admin.site.register(UpdateLog)
 admin.site.register(PageContent)
 admin.site.register(CanvasSite, CanvasSiteAdmin)
 admin.site.register(RequestSummary, RequestSummaryAdmin)
-
-# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Admin_site
-
-# https://medium.com/@hakibenita/how-to-turn-django-admin-into-a-lightweight-dashboard-a0e0bbf609ad
-
-# https://medium.com/crowdbotics/how-to-add-a-navigation-menu-in-django-admin-770b872a9531
