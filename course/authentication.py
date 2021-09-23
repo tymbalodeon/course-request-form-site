2c793548741082a56aa45a24d55df88d25e293cd
Ben Rosen <benrosen@upenn.edu>
2021-09-23 13:09:19 -0400

    fix auth

--- course/authentication.py
+++ course/authentication.py
@@ -1,17 +1,5 @@
 from django.contrib.auth.models import User
 
-from course import utils
-
-""""
-Since we are doing SSO authentication we need to
-
-
-idea from: https://stackoverflow.com/a/51994770/10969047
-but that requires backend changes
-
-doesnt require backend changes: https://stackoverflow.com/a/40894635/10969047
-"""
-
 
 class SSOLogin:
     def authenticate(self, username=None, password=None):
