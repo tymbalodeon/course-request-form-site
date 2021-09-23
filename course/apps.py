e7e526a9875c70457f5bff792fb95ca1c62f4f03
Ben Rosen <benrosen@upenn.edu>
2021-09-23 12:02:31 -0400

    testing change to app

--- course/apps.py
+++ course/apps.py
@@ -1,14 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
-
-from .signals import request_cleanup
 
 
 class CourseAppConfig(AppConfig):
     name = "course"
-    # verbose_name = "Course Request Form"
-
-    # not in use
-    def ready(self):
-        import course.signals  # noqa
