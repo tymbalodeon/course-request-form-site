4e42af22810f99423eff86a483c3e872b6d06928
Ben Rosen <benrosen@upenn.edu>
2021-09-23 13:07:39 -0400

    autocomplete

--- course/auto_complete.py
+++ course/auto_complete.py
@@ -4,8 +4,6 @@ from django.db.models import Q
 
 from course.models import CanvasSite, Subject
 
-# https://django-autocomplete-light.readthedocs.io/en/master/
-
 
 class UserAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
@@ -17,20 +15,14 @@ class UserAutocomplete(autocomplete.Select2QuerySetView):
         if not self.request.user.is_authenticated:
             return User.objects.none()
 
-        qs = User.objects.all()
+        query_set = User.objects.all()
+
         if self.q:
-            # lets limit it to only the first 8 matches
-            qs = qs.filter(username__istartswith=self.q)[:8]
+            return query_set.filter(username__istartswith=self.q)[:8]
         else:
-            # dont show all of them if there is nothing there
             return User.objects.none()
-        return qs
 
     def get_result_value(self, result):
-        """
-        this below is the default behavior,
-        change it to whatever you want returned
-        """
         return str(result.username)
 
 
@@ -39,22 +31,15 @@ class SubjectAutocomplete(autocomplete.Select2QuerySetView):
         if not self.request.user.is_authenticated:
             return Subject.objects.none()
 
-        qs = Subject.objects.all()
+        query_set = Subject.objects.all()
+
         if self.q:
-            # lets limit it to only the first 8 matches
-            qs = qs.filter(abbreviation__istartswith=self.q)[:8]
+            return query_set.filter(abbreviation__istartswith=self.q)[:8]
         else:
-            # dont show all of them if there is nothing there
             return Subject.objects.none()
-        return qs
 
     def get_result_value(self, result):
-        """
-        this below is the default behavior,
-        change it to whatever you want returned
-        """
-        # self.name, self.abbreviation
-        return str(result.abbreviation)  # + "("+str(result.name)+")"
+        return str(result.abbreviation)
 
 
 class CanvasSiteAutocomplete(autocomplete.Select2QuerySetView):
@@ -68,31 +53,21 @@ class CanvasSiteAutocomplete(autocomplete.Select2QuerySetView):
             return CanvasSite.objects.none()
 
         masquerade = self.request.session["on_behalf_of"]
+
         if masquerade:
-            qs = CanvasSite.objects.filter(
+            query_set = CanvasSite.objects.filter(
                 Q(owners=User.objects.get(username=masquerade))
                 | Q(added_permissions=self.request.user)
             )
         else:
-            qs = CanvasSite.objects.filter(
+            query_set = CanvasSite.objects.filter(
                 Q(owners=self.request.user) | Q(added_permissions=self.request.user)
             )
+
         if self.q:
-            # lets limit it to only the first 8 matches
-            qs = qs.filter(name__istartswith=self.q)[:8]
+            return query_set.filter(name__istartswith=self.q)[:8]
         else:
-            # dont show all of them if there is nothing there
-            return qs  # CanvasSite.objects.none()
-        return qs
+            return query_set
 
     def get_result_value(self, result):
-        """
-        this below is the default behavior,
-        change it to whatever you want returned
-        """
-
-        # self.name, self.abbreviation
-        return str(result.canvas_id)  # + "("+str(result.name)+")"
-
-
-# should have this for subjects,  and potentially content copies
+        return str(result.canvas_id)
