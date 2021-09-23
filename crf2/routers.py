0ddd408531587d7652d1151682dcd3b09bf6cdf0
Ben Rosen <benrosen@upenn.edu>
2021-09-23 11:57:04 -0400

    clean up utils

--- crf2/routers.py
+++ crf2/routers.py
@@ -1,5 +1,6 @@
-from course.viewsets import CourseViewSet
 from rest_framework import routers
+
+from course.viewsets import CourseViewSet
 
 router = routers.DefaultRouter()
 router.register("course", CourseViewSet)
