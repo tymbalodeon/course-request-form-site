21bb358e44382f0beafc64df44ea32a314a88198
Ben Rosen <benrosen@upenn.edu>
2021-09-23 13:02:45 -0400

    try naming permissions

--- course/context_processors.py
+++ course/context_processors.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 
 
-def user_permissons(request):
+def user_permissions(request):
     value = request.user.is_staff
 
     return {
