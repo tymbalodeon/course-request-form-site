fe546c32f95c63c9cd367b204d725d79a077153b
Ben Rosen <benrosen@upenn.edu>
2021-09-23 12:51:40 -0400

    update form

--- course/forms.py
+++ course/forms.py
@@ -11,10 +11,9 @@ from django.forms import (
     ValidationError,
 )
 
-from course.models import AdditionalEnrollment, CanvasSite, Subject
+from course.models import CanvasSite, Subject
 
 
-# our new form
 class ContactForm(Form):
     contact_name = CharField(required=True)
     contact_email = EmailField(required=True)
@@ -22,11 +21,9 @@ class ContactForm(Form):
 
 
 class UserForm(ModelForm):
-    # please only use this when you need to auto complete on the name field !
     username = ModelChoiceField(
         label="user",
         queryset=User.objects.all(),
-        # required=False,
         widget=autocomplete.ModelSelect2(url="user-autocomplete"),
     )
 
@@ -36,7 +33,6 @@ class UserForm(ModelForm):
 
 
 class SubjectForm(ModelForm):
-    # please only use this when you need to auto complete on the name field !
     abbreviation = ModelChoiceField(
         label="Abbreviation",
         queryset=Subject.objects.all(),
@@ -50,7 +46,6 @@ class SubjectForm(ModelForm):
 
 
 class CanvasSiteForm(ModelForm):
-    # please only use this when you need to auto complete on the name field !
     name = ModelChoiceField(
         label="content_copy",
         queryset=CanvasSite.objects.all(),
@@ -64,57 +59,53 @@ class CanvasSiteForm(ModelForm):
 
 
 class EmailChangeForm(Form):
-    """
-    A form that lets a user change set their email while checking for a change in the
-    e-mail.
-    """
-
     error_messages = {
         "email_mismatch": "The two email addresses fields didn't match.",
         "not_changed": "The email address is the same as the one already defined.",
     }
 
-    new_email1 = EmailField(
+    new_email_one = EmailField(
         label="New email address",
         widget=EmailInput,
     )
 
-    new_email2 = EmailField(
+    new_email_two = EmailField(
         label="New email address confirmation",
         widget=EmailInput,
     )
 
     def __init__(self, user, *args, **kwargs):
         self.user = user
-        # print(self.user)
         super(EmailChangeForm, self).__init__(*args, **kwargs)
 
     def clean_new_email1(self):
         old_email = self.user.email
-        new_email1 = self.cleaned_data.get("new_email1")
-        if new_email1 and old_email:
-            if new_email1 == old_email:
-                raise ValidationError(
-                    self.error_messages["not_changed"],
-                    code="not_changed",
-                )
-        return new_email1
+        new_email = self.cleaned_data.get("new_email1")
+
+        if new_email and old_email and new_email == old_email:
+            raise ValidationError(
+                self.error_messages["not_changed"],
+                code="not_changed",
+            )
+
+        return new_email
 
     def clean_new_email2(self):
-        new_email1 = self.cleaned_data.get("new_email1")
-        new_email2 = self.cleaned_data.get("new_email2")
-        if new_email1 and new_email2:
-            if new_email1 != new_email2:
-                # print("yeahgggggg")
-                raise ValidationError(
-                    self.error_messages["email_mismatch"],
-                    code="email_mismatch",
-                )
-        return new_email2
+        new_email_one = self.cleaned_data.get("new_email1")
+        new_email_two = self.cleaned_data.get("new_email2")
+        if new_email_one and new_email_two and new_email_one != new_email_two:
+            raise ValidationError(
+                self.error_messages["email_mismatch"],
+                code="email_mismatch",
+            )
+
+        return new_email_two
 
     def save(self, commit=True):
         email = self.cleaned_data["new_email1"]
         self.user.email = email
+
         if commit:
             self.user.save()
+
         return self.user
