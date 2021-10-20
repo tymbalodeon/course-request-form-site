e15bc99ced5f7d44f7f9d4f50a25a3542165e647
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-10-19 23:20:02 -0400

    remove logging

--- course/utils.py
+++ course/utils.py
@@ -1,20 +1,11 @@
 from __future__ import print_function
 
-import logging
-
 import cx_Oracle
 from canvas.api import create_canvas_user, get_canvas, get_user_by_sis, get_user_courses
 from django.db.models import Q
 from helpers.helpers import get_config_values
 
 from course.models import CanvasSite, Profile, Request, User
-
-logging.basicConfig(
-    filename="logs/users.log",
-    format="(%(asctime)s) %(levelname)s - %(message)s",
-    level=logging.DEBUG,
-    datefmt="%m/%d/%Y %I:%M:%S %p",
-)
 
 
 def data_warehouse_lookup(penn_key=None, penn_id=None):
