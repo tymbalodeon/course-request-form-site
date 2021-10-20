664a26a7eff52a56187cb0835d13ad605a3dd33e
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-10-19 23:18:24 -0400

    fix imports

--- crf2/routers.py
+++ crf2/routers.py
@@ -1,6 +1,5 @@
+from course.views import CourseViewSet
 from rest_framework import routers
-
-from course.viewsets import CourseViewSet
 
 router = routers.DefaultRouter()
 router.register("course", CourseViewSet)
