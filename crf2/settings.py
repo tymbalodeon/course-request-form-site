50575809c1fe7bc029c778ca6518ef171914a7c6
Ben Rosen <benrosen@upenn.edu>
2021-11-17 10:02:33 -0500

    fix livereload and update README

--- crf2/settings.py
+++ crf2/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "crf2.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [str(BASE_DIR / "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
