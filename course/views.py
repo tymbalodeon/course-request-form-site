fb54c5827998a88bec8207f03956813e620aeafc
Ben Rosen <benrosen@upenn.edu>
2022-01-11 21:49:05 -0500

    update log requests

--- course/views.py
+++ course/views.py
@@ -388,8 +388,11 @@ class RequestViewSet(MixedPermissionModelViewSet, ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         course = Course.objects.get(course_code=request.data["course_requested"])
         update_course(course)
-        logger.info(f"{self.request.user.get_username()} requesting course {course}:")
-        logger.info(f"{serializer.validated_data}")
+        logger.info
+            f'"{self.request.user.get_username()}" requesting course "{course}":'
+        )
+        for key, value in serializer.validated_data:
+            logger.info(f"{key}: {value}")
         if "view_type" in request.data:
             if request.data["view_type"] == "UI-course-list":
                 return redirect("UI-course-list")
