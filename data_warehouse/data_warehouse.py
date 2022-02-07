344aba1f30a8a688063cdd98cb12a9baee228b7f
Ben Rosen <benrosen@upenn.edu>
2022-02-07 10:08:02 -0500

    add underscore

--- data_warehouse/data_warehouse.py
+++ data_warehouse/data_warehouse.py
@@ -21,7 +21,7 @@ def get_cursor():
 
 
 def get_banner_course(srs_course_id, search_term):
-    srs_course_id = srs_course_id.replace(" ", "").replace("-", "").replace("-", "")
+    srs_course_id = srs_course_id.replace(" ", "").replace("-", "").replace("_", "")
     subject = "".join(character for character in srs_course_id if character.isalpha())
     if len(subject) == 3:
         srs_course_id = srs_course_id.replace(subject, f"{subject} ")
